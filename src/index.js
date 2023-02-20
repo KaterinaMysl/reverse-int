8cb69c5f78bd6d64b19dfb53daa830f6c670e328
KaterinaMysl <KaterinaMysl>
2023-02-20 11:57:32 +0300

    update index file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	let reverse = parseInt(Math.abs(n).toString().split('').reverse().join(''))
+	return reverse
+
 }
+console.log(module.exports(17))
